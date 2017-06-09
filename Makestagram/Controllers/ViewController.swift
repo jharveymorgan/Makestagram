ef403547b4ee6de9c45d6e9c69f8cc73646589fa
jharveymorgan <jharveymorgan11@gmail.com>
2017-06-09 16:34:19 -0400

    reorganize files

---
+++ Makestagram/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Jordan Harvey-Morgan on 6/9/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
