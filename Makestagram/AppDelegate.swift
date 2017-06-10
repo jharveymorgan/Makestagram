8c5ca06e59f04e8d10f99959d883703b1fa23fab
jharveymorgan <jharveymorgan11@gmail.com>
2017-06-09 23:54:00 -0400

    Add beginning of login UI

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -18,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // load login storyboard when user opens the app:
+        // instance of the Login storyboard
+        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        // if the story board has an initial view set, set it as the rootViewController and position it above any other views
+        if let initialViewController = storyboard.instantiateInitialViewController() {
+            window?.rootViewController = initialViewController
+            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
