893def3a4a418482cd31efc42b894cf8d745574f
jharveymorgan <jharveymorgan11@gmail.com>
2017-06-22 14:24:10 -0400

    Add custom fonts Add ability to convert hex to UIColor

---
+++ Makestagram/Extensions/UIFont+Utility.swift
@@ -0,0 +1,19 @@
+//
+//  UIFont+Utility.swift
+//  Makestagram
+//
+//  Created by Jordan Harvey-Morgan on 6/22/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+
+import UIKit.UIFont
+
+extension UIFont {
+    
+    // MARK: - Brand Fonts
+    
+    static var mgRegular: UIFont {
+        return UIFont(name: "OpenSans", size: 16)!
+    }
+}
